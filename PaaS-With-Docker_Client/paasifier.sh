1fc2534f66b437e0cd904e7737684ab945412ffa
vishnukumarnekkanti <vishnukumarnekkanti@gmail.com>
2015-11-22 19:04:03 +0530

    updating

--- PaaS-With-Docker_Client/paasifier.sh
+++ PaaS-With-Docker_Client/paasifier.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 if [ "$#" -gt 4	 ] || [ "$#" -le 0 ]
 then
-	echo "Usage: d_paas_cli deploy <repository:tag> <default app port> <serverip:port>"
+	echo "Usage:  paasifier.sh deploy <repository:tag> <default app port> <serverip:port>"
 	exit 1
 fi
 if ! [ "$1" == "deploy" ];
 then
-        echo "Usage: d_paas_cli deploy <repository:tag> <default app port> <serverip:port>"
+        echo "Usage: paasifier.sh deploy <repository:tag> <default app port> <serverip:port>"
         exit 1
 fi
 curl -X POST http://$4/deploy -H "Content-Type:application/json" -d '{"repo": "'$2'","port":"'$3'"}'
